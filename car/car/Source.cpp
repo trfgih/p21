861d65b444deb1ea235afed6d2873376591fc978
dima <dimabrovkin28538@gmail.com>
2024-04-17 20:54:52 +0500

    tank in progress

---
+++ car/car/Source.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+#define MIN_TANK_VOL 20
+#define MAX_TANK_VOL 120
+
+class tanc
+{
+	const int VOLUME;
+	double fuel_lvl;
+public:
+	const int get_VOLUME() const
+	{
+		return  VOLUME;
+	}
+	double get_fuel_lvl() const
+	{
+		return  fuel_lvl;
+	}
+	void fill(double amount)
+	{
+		if (amount < 0)return;
+		if (fuel_lvl + amount < VOLUME) fuel_lvl += amount;
+		else fuel_lvl = VOLUME;
+	}
+	double give_fuel(double amount)
+	{
+		if (amount < 0)return fuel_lvl;
+		fuel_lvl -= amount;
+		if (fuel_lvl < 0) fuel_lvl = 0;
+		return fuel_lvl;
+	}
+	tank(int volume): VOLUME(volume)
+	{
+		/*if (vol < MIN_TANK_VOL)vol = MIN_TANK_VOL;
+		if (vol < MAX_TANK_VOL)vol = MAX_TANK_VOL;*/
+		this->fuel_lvl = 0;
+		cout << "tanc is rady" << this << endl;
+	}
+	void info() const
+	{
+		cout << "tank volume" << VOLUME << "liters.\n";
+		cout << "fuel lvl" << fuel_lvl << "liters.\n";
+
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "");
+	tanc tank (-50);
+	tanc.info();
+}
