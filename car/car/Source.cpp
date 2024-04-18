9dad0c269edfde88e5e87e44b0859ae053dc8cdf
dima <dimabrovkin28538@gmail.com>
2024-04-18 18:42:59 +0500

    readme

--- car/car/Source.cpp
+++ car/car/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define MIN_TANK_VOL 20
 #define MAX_TANK_VOL 120
 
-class tanc
+class Tank
 {
 	const int VOLUME;
 	double fuel_lvl;
@@ -30,21 +30,34 @@ public:
 		if (fuel_lvl < 0) fuel_lvl = 0;
 		return fuel_lvl;
 	}
-	void tank(int volume): VOLUME(volume)
+	Tank(int volume)
+		:VOLUME(
+		volume<MIN_TANK_VOL?MIN_TANK_VOL:
+		volume>MAX_TANK_VOL?MAX_TANK_VOL:
+		volume)
 	{
 		this->fuel_lvl = 0;
 		cout << "tanc is rady" << this << endl;
 	}
 	void info() const
 	{
-		cout << "tank volume" << VOLUME << "liters.\n";
-		cout << "fuel lvl" << fuel_lvl << "liters.\n";
+		cout << "tank volume " << VOLUME << " liters.\n";
+		cout << "fuel lvl " << fuel_lvl << " liters.\n";
 
 	}
 };
 int main()
 {
 	setlocale(LC_ALL, "");
-	tanc tank (-50);
-	tanc.info();
+	Tank tank(85);
+	int fuel;
+	do
+	{
+		cout << "введите объем топлива: "; cin >> fuel;
+		tank.fill(fuel);
+		tank.info();
+
+	} while (fuel > 0);
+
 }
+
